e7119dfea779bdabf16fed993e42ddb6c75ba0ae
Liufengxuan <813949669@qq.com>
2022-10-14 14:14:59 +0800

    frame

---
+++ PPToDo/App.xaml.cs
@@ -0,0 +1,28 @@
+﻿using Prism.DryIoc;
+using Prism.Ioc;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace PPToDo
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : PrismApplication
+    {
+        protected override Window CreateShell()
+        {
+          return Container.Resolve<MainWindow>("MainWindow");
+        }
+
+        protected override void RegisterTypes(IContainerRegistry containerRegistry)
+        {
+           
+        }
+    }
+}
